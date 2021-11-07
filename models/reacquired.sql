08f18682f5793ccae2ffa1b7b3b69b84170692ee
raghulsuresh@zypmedia.com <raghulsuresh@zypmedia.com>
2021-11-07 16:49:26 +0000

    updated

---
+++ models/reacquired.sql
@@ -0,0 +1,20 @@
+-- using lag of last_month_status(to get users who had their first event in a previous month) to find the users who have reacquired in the current month and filtering days diffrence to one month. 
+-- This will output reacquired transitions with respective cohorts.
+
+with reacquired as (
+  select
+    MonthDate
+    , 'reacquired' as Last_month_status
+    , this_month_status
+    , count(user_id) as transitions
+  from
+    select * from {{ ref('aggregates') }}
+  where
+    Last_month_status_lag is null and diff_days < 35 and diff_days is not null
+  group by
+    1
+    , 2
+    , 3
+)
+
+select * from reacquired
